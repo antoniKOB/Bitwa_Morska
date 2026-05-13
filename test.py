744eb21457e7be3d8f311a2f84aa20458d5bcf8f
Bleblo430 <belowski2004@gmail.com>
2026-05-13 15:13:04 +0200

    aktualizacja 1

--- test.py
+++ test.py
@@ -7,12 +7,33 @@ print(text)
 letters = list(text)
 print(letters)
 if len(letters)>2:
-    print("wiadomośc musi być w formacie litera cyfra np B3")
+    print("wiadomośc za długa, wiadomość musi być w formacie litera cyfra np B3")
     
 
-def get_idx(letter):
+def letter2number(letter):
     letters = ["a","b","c","d","e","f","g","h","i","j"]
     idx = 1 + letters.index(letter)
     return idx
 
-print(get_idx(letters[0]))
+print(letter2number("a"))
+
+def get_idx(text):
+    text = text.replace(" ", "")
+    text = text.lower()
+    letters = list(text)
+    if len(letters)>2:
+        print("wiadomośc za długa, wiadomość musi być w formacie litera cyfra np B3")
+    else:
+        if(letters[0].isalpha() and letters[1].isdigit()):
+            i = letter2number(letters[1])
+            j = letters[0]
+        elif(letters[0].isdigit() and letters[1].isalpha()):
+            i = letter2number(letters[0])
+            j = letters[1]
+        else:
+            i = 0
+            j = 0
+            print("wiadomość musi być w formacie litera cyfra np. D6")
+    return i,j
+            
+            
